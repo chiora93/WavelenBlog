109bc516c432ba441ffde0e0b9c78113f9387502
SimoneChiorazzo <chiora93@gmail.com>
2015-01-23 22:42:10 +0100

    Fixati i settings dell'applicazione

--- wsgi/openshift/settings/development.py
+++ wsgi/openshift/settings/development.py
@@ -6,11 +6,21 @@ INTERNAL_IPS = ("127.0.0.1",)
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 ROOT_URLCONF = "urls"
 
+ALLOWED_HOSTS = [
+    '*.redhat.com',  # Allow domain and subdomains
+    '.blozzer.com.', # Also allow FQDN and subdomains
+]
+
+TIME_ZONE = 'Europe/Rome'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, '../../data/sqlite3.db'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mezzanine',
+        'USER': 'adminXsnWZBu',
+        'PASSWORD': 'jn3L8Dk_Piq6',
+        'HOST': '$OPENSHIFT_MYSQL_DB_HOST',
+        'PORT': '$OPENSHIFT_MYSQL_DB_PORT',
     }
 }
 
